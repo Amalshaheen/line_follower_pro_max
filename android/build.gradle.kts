785e2996a45c9b316a78ff5b4c60e4bea85e2661
Amal Shaheen <mkamalshaheen@gmail.com>
2026-03-26 08:25:20 +0530

    feat: Enhance build configuration to support dynamic namespace resolution from AndroidManifest

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,7 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.Project
+
 allprojects {
     repositories {
         google()
@@ -15,6 +19,43 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+fun Project.namespaceFromManifestOrFallback(): String {
+    val manifestFile = file("src/main/AndroidManifest.xml")
+    if (manifestFile.exists()) {
+        val packageName =
+            Regex("package=\\\"([^\\\"]+)\\\"")
+                .find(manifestFile.readText())
+                ?.groupValues
+                ?.getOrNull(1)
+                ?.trim()
+        if (!packageName.isNullOrEmpty()) {
+            return packageName
+        }
+    }
+
+    val normalizedName = name.replace('-', '_')
+    return "com.example.$normalizedName"
+}
+
+subprojects {
+    plugins.withId("com.android.application") {
+        extensions.configure<ApplicationExtension>("android") {
+            if (namespace.isNullOrBlank()) {
+                namespace = project.namespaceFromManifestOrFallback()
+            }
+        }
+    }
+
+    plugins.withId("com.android.library") {
+        extensions.configure<LibraryExtension>("android") {
+            if (namespace.isNullOrBlank()) {
+                namespace = project.namespaceFromManifestOrFallback()
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
